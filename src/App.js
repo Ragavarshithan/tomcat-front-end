0a4559b7c63df72349e3fa3e7534b6c9ff7c4922
Ragavarshithan <105372507+Ragavarshithan@users.noreply.github.com>
2023-09-08 00:56:19 +0530

    header & sidebar design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
+
 import './App.css'
-import MainDash from './components/pages/MainDash';
-import RightSide from './components/RigtSide/RightSide';
-import Sidebar from './components/Sidebar';
-import Performance from './components/performance';
+
+
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <div className="App">
-      <div className="AppGlass">
-        <Sidebar/>
-        <MainDash/>
-        <RightSide/>
-        
-      </div>
+      <TopNav/>
     </div>
   );
 }
